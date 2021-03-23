77fce583ad092671efa56a968363c41afc404767
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-23 16:15:27 +0600

    start express run time

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
-const app = express;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Thank you so much This awesome to learnign some new');
+});
+
+app.listen(3000, () => console.log('app is runing in port 3000'));
