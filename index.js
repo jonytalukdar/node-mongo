11c3a7f0073acbd39f799208d71c13d94a006072
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-23 17:16:39 +0600

    added dynamic api

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const users = ['jerry', 'joney', 'jubayer', 'roney'];
+
 app.get('/', (req, res) => {
-  res.send('Thank you so much This awesome to learnign some new');
+  res.send('Thank you so much this is very easy to learn this');
+});
+
+app.get('/fruits/banana', (req, res) => {
+  res.send({ fruit: 'banana', quantity: 1200, price: 1234 });
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  const name = users[id];
+  res.send({ id, name });
 });
 
 app.listen(3000, () => console.log('app is runing in port 3000'));
